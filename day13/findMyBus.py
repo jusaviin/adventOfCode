ad44a978d654a02c5dd96842e1aa99ca652caf32
Jussi Viinikainen <jviinika@uic.edu>
2020-12-14 10:53:43 -0600

    Debugging the solution for second part of puzzle

--- day13/findMyBus.py
+++ day13/findMyBus.py
@@ -3,6 +3,9 @@
 # Advent of code puzzle 13-1
 # Find the bus that comes first after arrivat at terminal
 
+# Advent of code puzzle 13-2
+# Find a timestamp from which each bus leaves following a given time interval
+
 #############################
 ##       Main program      ##
 #############################
@@ -16,10 +19,14 @@ arrivalTime = int(busNotes[0])
 
 busesInNotes = busNotes[1].split(",")
 buses = []
+timeIntervals = []
+originalIndex = -1
 for busNumber in busesInNotes:
+    originalIndex = originalIndex + 1
     if busNumber == "x":
         continue
     buses.append(int(busNumber))
+    timeIntervals.append(originalIndex)
 
 # Now that we know arrival time and bus schedules, check the waiting times for each
 waitingTimes = []
@@ -37,3 +44,18 @@ shortestIndex = waitingTimes.index(shortestTime)
 # Print the answer
 print("The shortest wait is {:d} minutes for bus {:d}".format(waitingTimes[shortestIndex], buses[shortestIndex]))
 print("When you multiply these numbers, you get {:d}".format(waitingTimes[shortestIndex]*buses[shortestIndex]))
+
+# Find the timestamp at which cool things happen!
+timestamp = 0
+notThereYet = True
+while notThereYet:
+    timestamp = timestamp + buses[0]
+    notThereYet = False
+    
+#    for i in range(1,len(buses)):
+#        if (timestamp + timeIntervals[i]) % buses[i] != 0:
+#            notThereYet = True
+#            break;
+        
+# Print the answer!
+print("Cool things happen at timestamp: {:d}".format(timestamp))
