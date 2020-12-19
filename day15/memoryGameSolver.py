d885ad95dcd52b31076305cc45e74dc99caf72a7
Jussi Viinikainen <jussi_viinikainen@hotmail.com>
2020-12-19 14:20:33 -0600

    About 30 % faster solving for puzzle 2

--- day15/memoryGameSolver.py
+++ day15/memoryGameSolver.py
@@ -27,21 +27,14 @@ def nextNumberSpoken(numbersSpoken):
     return "ERROR"
 
 # Fast algorithm to determine what is the next number based on the previous numbers
-def nextNumberSpokenQuick(numberDictionary):
-
-    # Find the current turn and the last spoken number
-    lastNumber = numberDictionary["last"]
+def nextNumberSpokenQuick(numberDictionary, lastNumber):
 
     # If the last number is spoken more than once, next number is the number of turns apart the number is from when it was previously spoken.
     if numberDictionary[lastNumber][0] > 0:
-        nextNumber = numberDictionary[lastNumber][1] - numberDictionary[lastNumber][0]
+        return numberDictionary[lastNumber][1] - numberDictionary[lastNumber][0]
 
     # If the last number has not been spoken before, the next number is zero
-    else:
-        nextNumber = 0
-
-    # Return the next number and whether is has been seen before
-    return nextNumber
+    return 0
 
 #############################
 ##       Main program      ##
@@ -59,15 +52,15 @@ while len(numbersSpoken) < 2020:
 print("The 2020th number spoken is {:d}".format(numbersSpoken[2019]))
 
 # Do the same but with an improved algorithm to get more easily into large numbers!
-numberDictionary = {6:(-1,1), 13:(-1,2), 1:(-1,3), 15:(-1,4), 2:(-1,5), 0:(-1,6), "last":0}
+numberDictionary = {6:(-1,1), 13:(-1,2), 1:(-1,3), 15:(-1,4), 2:(-1,5), 0:(-1,6)}
 
+nextNumber = 0
 for turn in range(7, 30000001):
-    nextNumber = nextNumberSpokenQuick(numberDictionary)
+    nextNumber = nextNumberSpokenQuick(numberDictionary, nextNumber)
     if nextNumber in numberDictionary:
         numberDictionary[nextNumber] = (numberDictionary[nextNumber][1], turn)
     else:
         numberDictionary[nextNumber] = (-1,turn)
-    numberDictionary["last"] = nextNumber
 
 # Print the answer!
 print("The 30000000th number spoken is {:d}".format(nextNumber))
